881f57415c1e5055219b6dcda0cda9b33a59808b
Eurípedes simões luz júnior <esimoeslj02@gmail.com>
2024-09-11 18:45:55 -0400

    Teste 01
    Testes

---
+++ teste/src/components/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="header">abidi hamudd</div>
+<main>
+    <section>Sessão 1</section>
+    <aside>Aside</aside>
+</main>
+<div>
+    <form>
+        <ul>
+            <li>li 1</li>
+            <li>li 2</li>
+            <li>li 3</li>
+        </ul>
+    </form>
+</div>
+</template>
+
+<script>
+export default{
+
+}
+
+</script>
+
+<style scoped>
+.header {
+    color: red;
+}
+li {
+    list-style: none;
+}
+</style>
