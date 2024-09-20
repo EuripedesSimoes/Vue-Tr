36d29accfa778668abb29c55f39c5a3fa318634f
Euripedes Simoes Luz Junior <esimoeslj02@gmail.com>
2024-09-20 12:00:06 -0300

    Create App

---
+++ app/src/components/TheCabc.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <p>Cabecalho 2</p>
+    </div>
+
+    <div v-if="user === 'adm'" >
+        <p>usuario é {{ user }}</p>
+    </div>
+    <div v-else>
+        <p>usuario esta como {{ user }}</p>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+
+user: 'am'
+            
+        }
+    }
+}
+
+</script>
+
+<style>
+p {
+    color: lightgreen;
+}
+
+</style>
---
+++ app/src/components/TheOne.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <button type="button" id="btn">Caliqu</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
+<style>
+#btn {
+    background-color: lightblue;
+}
+
+</style>
