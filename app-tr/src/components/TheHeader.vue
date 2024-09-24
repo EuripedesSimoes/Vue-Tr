636a662a744ff32ab21c4ccba75a5f59298c361c
Eurípedes simões luz júnior <esimoeslj02@gmail.com>
2024-09-24 16:56:22 -0400

    App-Tr

---
+++ app-tr/src/components/TheHeader.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+
+    </div> 
+    <hr style="width: 300px;">
+    <div>
+        <input type="text" v-model="div">
+        {{ div }}
+    </div>
+    <hr style="width: 300px;">
+    <label>Sports</label>
+    <select v-model="sport">
+        <option value="fut">futebas</option>
+        <option value="vol">volei</option>
+    </select>
+    <br>
+    {{ sport }}
+    <hr>
+    <div>
+        <p>Vc é yag?</p>
+        <input type="radio" value="sim" v-model="newsletter" >
+        <input type="radio" value="nao" v-model="newsletter" >
+        <input type="radio" value="talvez" v-model="newsletter" >
+        <br>
+        {{ newsletter }}
+    </div>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            div: 'Tuzin',
+            sport: 'Esportes',
+            newsletter: ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
