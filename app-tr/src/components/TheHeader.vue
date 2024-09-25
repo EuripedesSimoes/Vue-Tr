0b8fc768c141dc1c6ebfb7e456857042dd8fd588
Eurípedes simões luz júnior <esimoeslj02@gmail.com>
2024-09-25 16:51:04 -0400

    App-Tr
    Mais código

--- app-tr/src/components/TheHeader.vue
+++ app-tr/src/components/TheHeader.vue
@@ -7,7 +7,7 @@
         <input type="text" v-model="div">
         {{ div }}
     </div>
-    <hr style="width: 300px;">
+    <hr style="width: 320px;">
     <label>Sports</label>
     <select v-model="sport">
         <option value="fut">futebas</option>
@@ -15,7 +15,7 @@
     </select>
     <br>
     {{ sport }}
-    <hr>
+    <hr style="width: 340px;">
     <div>
         <p>Vc é yag?</p>
         <input type="radio" value="sim" v-model="newsletter" >
@@ -23,6 +23,14 @@
         <input type="radio" value="talvez" v-model="newsletter" >
         <br>
         {{ newsletter }}
+    </div>
+    <hr style="width: 360px;">
+    <div>
+        <button type="button" v-on:click="clicar()">Clique aqui</button>
+    </div>
+    <br>
+    <div class="red" v-on:mouseenter="dento()" v-on:mouseout="fora()">
+        Vermelho
     </div>
 
 </template>
@@ -33,12 +41,30 @@ export default {
         return {
             div: 'Tuzin',
             sport: 'Esportes',
-            newsletter: ''
+            newsletter: '',
+            clicar() {
+                alert('Você clicou, parabens zé')
+            },
+            dento(){
+                let dentoV = document.querySelector('.red')
+                dentoV.style.backgroundColor = 'lightblue'
+                dentoV.innerText = 'Ai dento'
+            },
+            fora(){
+                let dentoV = document.querySelector('.red')
+                dentoV.style.backgroundColor = 'violet'
+                dentoV.innerText = 'saiu'
+            }
         }
     }
 }
 </script>
 
 <style>
+.red {
+    width:  150px;
+    height: 150px;
+    background-color: red;
+}
 
 </style>
