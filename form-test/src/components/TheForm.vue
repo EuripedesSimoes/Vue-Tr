2b02962b503b5b419eeb1cf0a352a1271671f77a
Eurípedes simões luz júnior <esimoeslj02@gmail.com>
2024-10-23 16:52:39 -0400

    Form-Test

---
+++ form-test/src/components/TheForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <main class="main">
+        <div>
+            <form>
+                <input type="text" placeholder="Digite seu nome">
+                <input type="password" placeholder="Digite Sua senha">
+            </form>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+
+</script>
+
+<style>
+.main {
+    background-color: rgb(241, 192, 132);
+    width: 400px;
+    height: 400px;
+}
+</style>
