dcad69b93bc8ce24379c229e029fefeed503b6a8
Eurípedes simões luz júnior <esimoeslj02@gmail.com>
2024-11-07 11:57:46 -0400

    Form Test

--- form-test/src/components/TheForm.vue
+++ form-test/src/components/TheForm.vue
@@ -2,26 +2,29 @@
     <main class="main">
         <div class="div_main">
             <form class="form_name">
-                <input type="text" id="cu" v-model="txt" v-on:input="nameValidate()">
-                <span id="span_required" :style="display_Span" >Senha deve ter 8 caracteres</span>
+                <input type="text" id="cu" v-model="txt" v-on:input="nameValidate">
+                <span id="span_required" :style="display_Span" >Senha deve ter 3 caracteres</span>
+                {{ txt.length }}
             </form>
 
-            <form class="form_pass">
+            <!--<form class="form_pass">
                 <input type="password" placeholder="Digite Sua senha">
-                <span id="span_required" style="display: flex; color: red;">Senha deve ter 5 caracteres</span>
+                <span id="span_required2" style="display: flex; color: red;">Senha deve ter 5 caracteres</span>
             </form>
 
-            <!--
-            <form>
-                <input type="text" placeholder="Digite Sua senha" v-model="ip3">
-                <span id="span_required" v-if="ip3.length = 0" style="display: none;"></span>
-                <span id="span_required" v-else-if="ip3.length < 5" style="display: flex; color: red;">Senha deve ter 5 caracteres</span>
-                <span id="span_required" v-else-if="ip3.length >= 5" style="display: flex; color: green;">Senha deve ter 5 caracteres</span>
-            </form>-->
+            
+                <span id="span_required" v-if="txt.length = 0" style="display: none;"></span>
+                -->
+                <form>
+                <input type="text" placeholder="Digite Sua senha" v-model="txt">
+                <span id="span_required3" v-if="txt.length < 5" style="display: flex; color: red;">Senha deve ter 5 caracteres</span>
+                <span id="span_required4" v-else-if="txt.length >= 5" style="display: flex; color: green;">Senha com caracteres certo</span>
+            </form>
         </div>
         <div>
-            <input type="radio" value="uc">cuz
-            <input type="radio" value="niz">in
+            <input type="radio" value="pass" v-model="txt">pass
+            <input type="radio" value="password" v-model="txt">password
+            <p>Você selecionou  {{ txt }}</p>
         </div>
     </main>
 </template>
@@ -42,13 +45,14 @@ export default {
         nameValidate(){
             console.log(this.txt)
 
-            if (this.txt.length < 3){
-                this.display_Span = 'display: flex; color:red'
+            if (this.txt.length < 5){
+                this.display_Span = 'display: block; color:red'
                 
             }
             
             else {
-                    this.display_Span = 'display: flex; color:green'
+                    this.display_Span = 'display: block; color:green'
+                    /*this.spanT.innerText = 'Caracteres certos'*/
                 }
             
             /*if (this.txt < 5)
