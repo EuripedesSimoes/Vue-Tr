039f59e8a1a828ae774e00c448506282c47cb31a
Euripedes Simoes Luz Junior <esimoeslj02@gmail.com>
2024-11-23 23:11:18 -0300

    Burger-Project

---
+++ burger/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+  <Footer_1></Footer_1>
+</template>
+
+
+<script>
+import Footer_1 from "./components/TheFooter.vue";
+
+export default{
+  name:"App",
+  data(){
+    return {
+
+    }
+  },
+  components: {
+    Footer_1
+  }
+}
+</script>
+
+
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
