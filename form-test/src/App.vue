2b281f8aff6aa92f828cefc3682c9532178eb04f
Eurípedes simões luz júnior <esimoeslj02@gmail.com>
2024-12-10 17:07:03 -0400

    Form-Test
    Add navbar

--- form-test/src/App.vue
+++ form-test/src/App.vue
@@ -1,17 +1,20 @@
 <template>
+  <burger_esboço/>
   <TheForm2></TheForm2>
 </template>
 
 <script>/*
 import FormEscalacao from './components/TheForm.vue';*/
 import TheForm2 from './components/TheForm2.vue';
+import burger_esboço from './components/Navbar.vue';
 
 
 export default {
   name: 'App',
   components: {
    /* FormEscalacao,*/
-    TheForm2
+    TheForm2,
+    burger_esboço
   }
 }
 </script>
---
+++ form-test/src/components/Navbar.vue
@@ -0,0 +1,79 @@
+<template>
+
+    <nav class="nav_bar">
+         <div class="nav_logo">
+            <img src="/assets/logo.png">
+        </div> //tem que ser uns 70px
+         <div class="nav_list">
+        <ul class="ul_nav">
+            <li>Home</li>
+            <li>Sobre</li>
+            <li>Produtos</li>
+            <li>Contato</li>
+        </ul>
+         </div>
+    </nav>
+    
+    </template>
+    
+    
+    
+    
+    <script>
+    export default {
+        name: 'burger_esboço',
+    data() {
+        
+        return {
+            
+    
+        }
+        },
+    
+    }
+    
+    </script>
+    
+    
+    
+    
+    <style scoped>
+    
+    .nav_bar {
+    background: red;
+    display: flex;
+    justify-content: center;
+    align-items: space-around;
+    width: 100vh;
+    height: 200px;
+    }
+    
+    .nav_logo {
+    background: blue;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100;
+    height: 120px;
+    }
+    
+    .nav_list {
+    background: green;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 400px;
+    height: 120px;
+    }
+    
+    .ul_nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    }
+    .ul_nav li {
+    color: white;
+    font-size: 16px;
+    }
+    
+    </style>
