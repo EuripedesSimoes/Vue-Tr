fb5a929f4344c5eac9b1770943da858d7dc65cee
Euripedes Simoes Luz Junior <esimoeslj02@gmail.com>
2024-12-26 23:21:13 -0300

    Burger

--- burger/src/views/PedidosView.vue
+++ burger/src/views/PedidosView.vue
@@ -6,16 +6,19 @@
         </nav>
 
 
-
         <div class="div_lunch">
             <div class="lunch">
-                <img src="../assets/hamburger_128px.png" alt="">
-                <p>X-Salada</p>
-                <ul>
-                    <li id="pao_lanche">Pão de Brioche</li>
-                    <li id="carne_lanche">Carne</li>
-                    <li id="salada_lanche">Salada</li>
-                </ul>
+                <div class="div_img">
+                    <img src="../assets/pedidos/pedidos_burger/x-salada.jpeg" alt="">
+                </div>
+                <div class="div_desc">
+                    <p>X-Salada</p>
+                    <ul>
+                        <li id="pao_lanche">Pão de Brioche</li>
+                        <li id="carne_lanche">Carne</li>
+                        <li id="salada_lanche">Salada</li>
+                    </ul>
+                </div>
                 
 
             </div>
@@ -48,7 +51,8 @@
 }
 .peidos {
     margin: 0 auto;
-    background-color: rgb(129, 39, 39);
+    background-color: #dddddd95;
+    backdrop-filter: blur(5px);
     color: white;
     height: 75vh;
     width: 900px;
@@ -65,15 +69,57 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    column-gap: 30px;
+    column-gap: 20px;
 }
 .lunch{
-    border-right: 3px solid black;
+    border: 3px solid black;
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 300px;
-    width: 300px;
+    height: 250px;
+    width: 400px;
     background-color: wheat;
+    /* column-gap: 30px; */
 }
+.div_img {
+    background-color: black;
+    height: 100%;
+    width: 65%;
+    img{
+        height: 100%;
+        width: 100%;
+    }
+}
+
+.div_desc {
+    height: 100%;
+    width: 35%;
+    background-color: black;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+    row-gap: 30px;
+    p {
+        margin-top: 20px;
+        font-size: 28px;
+        background-color: #FF9900;
+        color: black;
+        font-weight: bolder;
+        padding: 3px;
+        border-radius: 10px;
+    }
+    ul {
+        margin-left: 15px;
+        display: flex;
+        flex-direction: column;
+        row-gap: 10px;
+        align-items: start;
+        list-style: square;
+    }
+}
+/* #pao_lanche {
+    list-style: square;
+} */
+
 </style>
