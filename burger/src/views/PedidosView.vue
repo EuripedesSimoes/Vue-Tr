2104da40f21a235a8ffad255f63f3e0ef739ae34
Eurípedes simões luz júnior <esimoeslj02@gmail.com>
2025-01-04 11:48:20 -0400

    Burger

--- burger/src/views/PedidosView.vue
+++ burger/src/views/PedidosView.vue
@@ -69,9 +69,16 @@ export default {
         const req_final = await fetch("http://localhost:3000/burgers")
         const data = await req_final.json()
 
+    //         const paes = data.paes;
         this.burger_SS = data.All_Burgers
         console.log(this.burger_SS)
-    
+    //         paes.forEach(pao => {
+    //             const div = document.createElement('div');
+    //             div.textContent = `ID: ${pao.id}, Tipo: ${pao.tipo}`;
+    //             document.body.appendChild(div);
+    //         });
+        this.burger_SS.forEach((burgin_SS) => { 
+            // alert( burgin_SS.img_src)
 
         //identifica o container para criar os elementos
         const divMainlunch = document.getElementById('post-containerr')
@@ -86,14 +93,17 @@ export default {
         
         //criando os elementos de DIV e P ---- 2 DIV
         const divContainerDesc = document.createElement('div')
-        const p = document.createElement('p')
-        p.innerText = 'DIV tetse final'
-        const ul = document.createElement('ul')
-        const li = document.createElement('li')
+        const p_brg = document.createElement('p')
+        p_brg.innerText = 'DIV tetse final'
+        const ul_brg = document.createElement('ul')
+        const li_brg = document.createElement('li')
 
         //cuidando da IMG
         // img.setAttribute('class', 'imgFromLunch')
-        img_brg.src = 'https://www.comidaereceitas.com.br/wp-content/uploads/2008/06/HAMBURGUER-VEGETARIANO-780x439.jpg'
+            img_brg.src = burgin_SS.img_src  
+    // "../assets/pedidos/pedidos_burger/x-salada.jpeg"
+
+        // img_brg.src = 'https://www.comidaereceitas.com.br/wp-content/uploads/2008/06/HAMBURGUER-VEGETARIANO-780x439.jpg'
         // img_brg.style.height = '100%'
         // img_brg.style.width = '100%'
 
@@ -104,9 +114,9 @@ export default {
         divContainerDesc.className = 'div_desc'
 
         //Colocando texto no P e colocando o P dentro da DIV
-        divContainerDesc.appendChild(p)
-        ul.appendChild(li)
-        divContainerDesc.appendChild(ul)
+        divContainerDesc.appendChild(p_brg)
+        ul_brg.appendChild(li_brg)
+        divContainerDesc.appendChild(ul_brg)
 
         //colocando a DIV criada dentro da DIV principal
         div2lunch.appendChild(divContainerImg)
@@ -114,7 +124,7 @@ export default {
 
         divMainlunch.appendChild(div2lunch)
         
-
+    })
     
     //   const postContainer = document.getElementById('post-container-final')
 
@@ -136,29 +146,55 @@ export default {
     }
     },
     mounted() {
-        async function calabreso(){
-            const req_calabreso = await fetch("http://localhost:3000/burgers")
-            const data_calabreso = await req_calabreso.json()
+    //     async function calabreso(){
+    //         const req_calabreso = await fetch("http://localhost:3000/burgers")
+    //         const data_calabreso = await req_calabreso.json()
 
-            let burger_array = null
+    //         let burger_array = null
 
-            burger_array = data_calabreso.All_Burgers
-            console.log(burger_array.push())
-            burger_array.forEach((_, i)=>{
-            let ind = i
-            console.log(ind)
-            // this.cuz()*ind
-            // for (let i = 0; i < 5; i++){
-            //     this.cuz()
-            // }
-            while (ind === true){
-                console.log('cu')
-            }
-        })
-        }
-
-        
-        calabreso(this.cuz())
+    //         burger_array = data_calabreso.All_Burgers
+    //         console.log(burger_array.push())
+    //         burger_array.forEach((_, i)=>{
+    //             let ind = i
+    //         console.log(ind)
+    //         // this.cuz()*ind
+    //         // for (let i = 0; i < 5; i++){
+    //         //     this.cuz()
+    //         // }
+    //         while (ind === true){
+    //             console.log('cu')
+    //         }
+    //     })
+    // }
+  // URL do banco de dados JSON
+    // const url = 'https://exemplo.com/banco-de-dados.json';
+    
+    // // Função para fazer a requisição fetch e processar os dados
+    // async function fetchAndCreateDivs() {
+    //     try {
+    //         const response = await fetch(url);
+    //         const data = await response.json();
+    
+    //         const paes = data.paes;
+    
+    //         // Percorre os itens do array e cria uma div para cada id
+    //         paes.forEach(pao => {
+    //             const div = document.createElement('div');
+    //             div.textContent = `ID: ${pao.id}, Tipo: ${pao.tipo}`;
+    //             document.body.appendChild(div);
+    //         });
+    
+    //     } catch (error) {
+    //         console.error('Erro ao buscar os dados:', error);
+    //     }
+    // }
+    
+    // // Chama a função para executar a requisição e criação das divs
+    // fetchAndCreateDivs(); 
+    
+    
+        // calabreso()
+        this.cuz()
 }
 }
 
@@ -224,29 +260,4 @@ export default {
 
 </style>
 
-<!-- // URL do banco de dados JSON
-const url = 'https://exemplo.com/banco-de-dados.json';
-
-// Função para fazer a requisição fetch e processar os dados
-async function fetchAndCreateDivs() {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-
-        const paes = data.paes;
-
-        // Percorre os itens do array e cria uma div para cada id
-        paes.forEach(pao => {
-            const div = document.createElement('div');
-            div.textContent = `ID: ${pao.id}, Tipo: ${pao.tipo}`;
-            document.body.appendChild(div);
-        });
-
-    } catch (error) {
-        console.error('Erro ao buscar os dados:', error);
-    }
-}
-
-// Chama a função para executar a requisição e criação das divs
-fetchAndCreateDivs(); -->
 
